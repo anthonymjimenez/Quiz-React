c87731b6e959be0599c3f6da0f7682b9f8f1a03f
securethebags <anthonyjim96@gmail.com>
2020-05-08 20:15:46 -0400

    updated scoreboard: fixed styling, display inline-block

--- src/Quiz/QuizComponents/Quiz.js
+++ src/Quiz/QuizComponents/Quiz.js
@@ -4,24 +4,22 @@ import { Question }  from './Question';
 import { ScoreBoard } from './ScoreBoard';
 import axios from 'axios';
 
-
-
 export const Quiz = () => {
   // hooks used in relation to API Call
     const [questionData, setQuestionData] = useState([]) 
     const [answerData, setAnsData] = useState([])
     const [correctAnsData, setCorrectAnsData] = useState([])
     const [loading, setLoading] = useState(true)
-
+    var [ difficulty, setDifficulty ] = useState('hard')
+    var [category, setCategory] = useState('computers')
+    
   // hooks used for game logic
     var [ index, setIndex ] = useState(0)
     var [ result, setResult ] = useState(null)
-    var [ difficulty, setDifficulty ] = useState('hard')
-    var [category, setCategory] = useState('Computers')
     var [right, setRight] = useState(0)
 
 
-    const getData = async (diff, cat) => { //API call to a triva database
+  const getData = async (diff, cat) => { //API call to a trivia database (diff, cat) -> strings that determine API call
         setLoading(true)
         const incomingData = await axios.get(
         `https://opentdb.com/api.php?amount=10&category=${cat}&difficulty=${diff}&type=multiple`);
@@ -30,40 +28,31 @@ export const Quiz = () => {
         setAnsData(incomingData.data.results.map(({ incorrect_answers }) => incorrect_answers))
         setCorrectAnsData(incomingData.data.results.map(({ correct_answer }) => correct_answer)) 
   };
+    
+  function easyDifficulty() {setDifficulty('easy')}
+  function mediumDifficulty() {setDifficulty('medium')}
+  function hardDifficulty() {setDifficulty('hard')}
+
+  function categoryVG() {setCategory('video Games')}
+  function categoryComps() { setCategory('computers')}
+  function categorySports() {setCategory('sports')}
+  function categoryBooks()  { setCategory('books')}
 
     useEffect(() => {
       // this useEffect listens for changes to difficulty and or category
       var categoryNumber;
 
-      if(category === "Video Games") { categoryNumber = 15}
-      else if(category === "Computers") { categoryNumber = 18}
-      else if(category === "Sports") { categoryNumber = 21}
-      else if(category === "Books") { categoryNumber = 10}
+      if(category === "video Games") { categoryNumber = 15}
+      else if(category === "computers") { categoryNumber = 18}
+      else if(category === "sports") { categoryNumber = 21}
+      else if(category === "books") { categoryNumber = 10}
       // the api defines category in terms of numbers, this is how we get the current category to be a relevent string
 
       getData(difficulty, categoryNumber)
-      // the call to getData makes a request on the triva API that implements template strings to 
-      // easily change the API request to match user input
+      // the call to getData makes a request on the triva API that implements template strings that
+      // easily changes the API request to match user input
     },[difficulty, category])
-    
-    function easyDifficulty() {
-      setDifficulty('easy')
-    }
-    function mediumDifficulty() {
-      setDifficulty('medium')
-    }
-    function hardDifficulty() {
-      setDifficulty('hard')
-    }
-
-    function categoryVG() {setCategory('Video Games')}
-    function categoryComps() { setCategory('Computers')}
-    function categorySports() {setCategory('Sports')}
-    function categoryBooks()  { setCategory('Books')}
  
-
- 
-
     useEffect(() => {if(index === questionData.length || loading === true) { 
       // useEffect to reset the values that scoreboard implements
       setIndex(0)
@@ -73,32 +62,30 @@ export const Quiz = () => {
    
 
     if (loading) return "loading...";
-    return ( 
-    
+
+    return (     
         <>
-        <h1>Created by Anthony Jimenez</h1>
+        <h2>Created by Anthony Jimenez</h2>
 
-
-        <div className = 'float half'>
-        <p>Current difficulty: {difficulty}</p>
+        <div className = 'toggle half'>
         <p>Choose difficulty!</p> 
+        <p>Current difficulty: {difficulty}</p>
         <button onClick={easyDifficulty}>Easy</button>
         <button onClick={mediumDifficulty}>Medium</button>
         <button onClick={hardDifficulty}>Hard</button>
         </div>
 
-        <div className = 'float half'>
-        <p>Current category: {category}</p>
+        <div className = 'toggle half'>
         <p>Choose category!</p>
+        <p>Current category: {category}</p>
         <button onClick = {categoryVG}>Video Games</button>
         <button onClick = {categoryComps}>Computers</button>
         <button onClick = {categorySports}>Sports</button>
         <button onClick = {categoryBooks}> Books</button>
         </div>
-        
-        <ScoreBoard result={result} index={index} setRight = {setRight} right = {right} />
 
         <div className = 'questions'>
+        <ScoreBoard result={result} index={index} setRight = {setRight} right = {right} />
         <Question Questions={questionData[index]}/>
         <AnswerList answers={answerData[index]} correctAns ={correctAnsData[index]} index = {index} setResult = {setResult} setIndex = {setIndex}/>
         </div>
